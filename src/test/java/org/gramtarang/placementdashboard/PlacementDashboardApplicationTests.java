afae801f22dafbf903ce17119e8bdc3bfb3b3517
Harendra@84 <harendrakumar843334@gmail.com>
2023-09-27 14:35:35 +0530

    hii

---
+++ src/test/java/org/gramtarang/placementdashboard/PlacementDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gramtarang.placementdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlacementDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
